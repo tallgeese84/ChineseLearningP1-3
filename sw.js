9247753e22405e6649590dd251efadb98dffeff4
WandererLim <26509069+tallgeese84@users.noreply.github.com>
2026-08-01 13:18:18 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "huawen-v4";
+const CACHE = "huawen-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
